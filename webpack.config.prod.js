3670b531bca65759be9757e2749cc0fe10db78ec
K Glass <kglass@cafex.com>
2021-05-17 09:34:05 +0100

    Update to include minified source if you're that way inclined

---
+++ webpack.config.prod.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './lib',
+  },
+  output: {
+    filename: 'index-min.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: "gute",
+    libraryTarget: 'umd'
+  }
+};
